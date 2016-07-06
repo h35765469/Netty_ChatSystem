93a32ec40f8b971bdc6e214af498d595035db389
evility <h35765469@gmail.com>
2016-07-06 18:54:35 +0800

    創建處理離線訊息的DTO

---
+++ app/src/main/java/com/example/user/netty_chatsystem/Chat_server/dto/OfflineMessageDTO.java
@@ -0,0 +1,33 @@
+package com.example.user.netty_chatsystem.Chat_server.dto;
+
+
+import com.example.user.netty_chatsystem.Chat_biz.entity.OfflineMessage;
+import com.example.user.netty_chatsystem.Chat_core.transport.DataBuffer;
+import com.example.user.netty_chatsystem.Chat_core.transport.IMSerializer;
+
+/**
+ * Created by user on 2016/7/6.
+ */
+public class OfflineMessageDTO implements IMSerializer {
+
+    OfflineMessage offlineMessage;
+
+   public OfflineMessageDTO(OfflineMessage offlineMessage){
+       this.offlineMessage = offlineMessage;
+   }
+
+
+    public DataBuffer encode(short version) {
+        DataBuffer buffer = new DataBuffer();
+        buffer.writeStringArray(offlineMessage.getOfflineMessageArray());
+        return buffer;
+    }
+
+
+    public void decode(DataBuffer buffer, short version) {
+        if(offlineMessage == null){
+            offlineMessage = new OfflineMessage();
+        }
+        offlineMessage.setOfflineMessageArray(buffer.readStringArray());
+    }
+}
