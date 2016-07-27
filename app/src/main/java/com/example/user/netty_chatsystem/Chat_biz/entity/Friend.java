607dab68306d1d668c5ed996f7275ed8b0d5b9c8
evility <h35765469@gmail.com>
2016-07-27 10:24:16 +0800

    處理各項朋友資料的class

---
+++ app/src/main/java/com/example/user/netty_chatsystem/Chat_biz/entity/Friend.java
@@ -0,0 +1,74 @@
+package com.example.user.netty_chatsystem.Chat_biz.entity;
+
+/**
+ * Created by user on 2016/7/21.
+ */
+public class Friend {
+    private String friendUserName;
+    private String friendName;
+    private String username;
+    private String[] friendArray;
+    private int isFavorite = 0;
+    private int isBlock = 0;
+
+
+    public Friend(){
+
+    }
+
+    public Friend(String username , String friendUserName){
+        this.username = username;
+        this.friendUserName = friendUserName;
+    }
+
+    public String getFriendUserName(){
+        return friendUserName;
+    }
+
+    public void setFriendUserName(String friendUserName){
+        this.friendUserName = friendUserName;
+    }
+
+    public String getFriendName(){
+        return friendName;
+    }
+
+    public void setFriendName(String friendName){
+        this.friendName = friendName;
+    }
+
+    public String getUserName(){
+        return username;
+    }
+
+    public void setUserName(String username){
+        this.username = username;
+    }
+
+    public String[] getFriendArray(){
+        return friendArray;
+    }
+
+    public void setFriendArray(String[] friendArray){
+        this.friendArray = friendArray;
+    }
+
+
+    public int getIsFavorite(){
+        return isFavorite;
+    }
+
+    public void setIsFavorite(int isFavorite){
+        this.isFavorite = isFavorite;
+    }
+
+    public int getIsBlock(){
+        return isBlock;
+    }
+
+    public void setIsBlock(int isBlock){
+        this.isBlock = isBlock;
+    }
+
+
+}
