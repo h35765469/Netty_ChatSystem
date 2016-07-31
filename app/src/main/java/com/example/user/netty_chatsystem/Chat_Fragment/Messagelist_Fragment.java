3c4a165131d00cd8ec12ce0b4039f5305ace748e
evility <h35765469@gmail.com>
2016-07-31 15:52:23 +0800

    填加 處理訊息來時新增到listview  public void updateMessage(RowItem rowItem){        if(!messageAdapter.inMessage(rowItem)) {            messageAdapter.addMessage(rowItem);        }        messageAdapter.notifyDataSetChanged();        scroll();    }     private void scroll() {         Messagelist_listview.setSelection(Messagelist_listview.getCount() - 1);     }
       專門更新介面的handler
         Handler messageHandler = new Handler(){
                @Override
                public void handleMessage(Message msg)
                {
                    super.handleMessage(msg);
    
                    RowItem rowItem = (RowItem)msg.getData().getSerializable("updateMessage");
                    updateMessage(rowItem);
                }
            };
    
        從廣播中獲取資料
          chatBroadcastReceiver.setChangeMessageListListener(new ChatBroadcastReceiver.ChangeMessageListListener() {
                    @Override
                    public void onChangeMessageListEvent(String from, String content) {
                        RowItem rowItem = new RowItem(images[0],from,from);
    
                        Bundle bundle = new Bundle();
                        bundle.putSerializable("updateMessage", rowItem);
                        Message msg = new Message();
                        msg.setData(bundle);
                        messageHandler.sendMessage(msg);
                    }
                });

--- app/src/main/java/com/example/user/netty_chatsystem/Chat_Fragment/Messagelist_Fragment.java
+++ app/src/main/java/com/example/user/netty_chatsystem/Chat_Fragment/Messagelist_Fragment.java
@@ -2,6 +2,8 @@ package com.example.user.netty_chatsystem.Chat_Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -10,12 +12,12 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import com.example.user.netty_chatsystem.Character_Activity;
 import com.example.user.netty_chatsystem.Chat_Activity;
 import com.example.user.netty_chatsystem.Chat_Listview_Friendlist.CustomBaseAdapter;
 import com.example.user.netty_chatsystem.Chat_Listview_Friendlist.RowItem;
+import com.example.user.netty_chatsystem.Chat_Service.ChatBroadcastReceiver;
 import com.example.user.netty_chatsystem.R;
 import com.example.user.netty_chatsystem.Search_Activity;
 
@@ -33,23 +35,49 @@ public class Messagelist_Fragment extends BaseFragment {
 
 
     public static final Integer[] images = { R.drawable.bomb,
-            R.drawable.search, R.drawable.desk, R.drawable.color_white };
+            R.drawable.search, R.drawable.bomb, R.drawable.color_white };
 
     public String [] Id_array = {"123" , "456","789","465"};
 
+    ChatBroadcastReceiver chatBroadcastReceiver = new ChatBroadcastReceiver();
 
+    CustomBaseAdapter messageAdapter;
+
+    ListView Messagelist_listview;
+
+    Handler messageHandler = new Handler(){
+        @Override
+        public void handleMessage(Message msg)
+        {
+            super.handleMessage(msg);
+
+            RowItem rowItem = (RowItem)msg.getData().getSerializable("updateMessage");
+            updateMessage(rowItem);
+        }
+    };
 
     @Override
     public View initView(LayoutInflater inflater){
         View view = inflater.inflate(R.layout.activity_messagelist_,null);
         setHasOptionsMenu(true);
 
+        chatBroadcastReceiver.setChangeMessageListListener(new ChatBroadcastReceiver.ChangeMessageListListener() {
+            @Override
+            public void onChangeMessageListEvent(String from, String content) {
+                RowItem rowItem = new RowItem(images[0],from,from);
 
+                Bundle bundle = new Bundle();
+                bundle.putSerializable("updateMessage", rowItem);
+                Message msg = new Message();
+                msg.setData(bundle);
+                messageHandler.sendMessage(msg);
+            }
+        });
 
 
         final ImageView Messagelist_character_imageview = (ImageView)view.findViewById(R.id.friendlist_addfriend_imageview);
         final ImageView Messagelist_search_imageview = (ImageView)view.findViewById(R.id.messagelist_search_imageview);
-        final ListView Messagelist_listview = (ListView)view.findViewById(R.id.messagelist_listview);
+        Messagelist_listview = (ListView)view.findViewById(R.id.messagelist_listview);
 
 
         List<RowItem>Rowitem_list = new ArrayList<RowItem>();
@@ -58,7 +86,7 @@ public class Messagelist_Fragment extends BaseFragment {
             Rowitem_list.add(Rowitem);
         }
 
-        CustomBaseAdapter adapter = new CustomBaseAdapter(getActivity(),Rowitem_list);
+        messageAdapter = new CustomBaseAdapter(getActivity(),Rowitem_list);
 
         Messagelist_search_imageview.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -79,7 +107,7 @@ public class Messagelist_Fragment extends BaseFragment {
 
 
         //設置處理message的訊息列條監聽
-        Messagelist_listview.setAdapter(adapter);
+        Messagelist_listview.setAdapter(messageAdapter);
         Messagelist_listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -122,4 +150,15 @@ public class Messagelist_Fragment extends BaseFragment {
    @Override
     public void onPrepareOptionsMenu(Menu menu){
    }
+
+   public void updateMessage(RowItem rowItem){
+       if(!messageAdapter.inMessage(rowItem)) {
+           messageAdapter.addMessage(rowItem);
+       }
+       messageAdapter.notifyDataSetChanged();
+       scroll();
+   }
+    private void scroll() {
+        Messagelist_listview.setSelection(Messagelist_listview.getCount() - 1);
+    }
 }
