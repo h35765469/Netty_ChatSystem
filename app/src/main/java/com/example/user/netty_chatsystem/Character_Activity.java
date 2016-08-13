23c8cbd26051c96908d8009143ba5b2493b24b3c
evility <h35765469@gmail.com>
2016-08-13 10:06:02 +0800

    更改mFragmentList.add()的順序
      mFragmentList.add(new Profile_Fragment());
            mFragmentList.add(new Character_Fragment());
            mFragmentList.add(new Friendlist_Fragment());
            mFragmentList.add(messagelist_fragment);

--- app/src/main/java/com/example/user/netty_chatsystem/Character_Activity.java
+++ app/src/main/java/com/example/user/netty_chatsystem/Character_Activity.java
@@ -1,29 +1,27 @@
 package com.example.user.netty_chatsystem;
 
 
-
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.example.user.netty_chatsystem.Chat_Fragment.Character_Fragment;
 import com.example.user.netty_chatsystem.Chat_Fragment.Friendlist_Fragment;
 import com.example.user.netty_chatsystem.Chat_Fragment.Messagelist_Fragment;
+import com.example.user.netty_chatsystem.Chat_Fragment.Profile_Fragment;
 import com.example.user.netty_chatsystem.Chat_Fragment.ViewPagerAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Character_Activity extends AppCompatActivity {
+public class Character_Activity extends FragmentActivity {
     public static ViewPager mViewPager;
     private List<Fragment>mFragmentList = new ArrayList<Fragment>();
     private Messagelist_Fragment messagelist_fragment;
     private ViewPagerAdapter mViewPagerAdapter;
-
 
 
 
@@ -47,20 +45,21 @@ public class Character_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_template);
-        this.getSupportActionBar().hide();
         mViewPager = (ViewPager)findViewById(R.id.container);
-        init();
+        init(savedInstanceState);
     }
 
-    private void init(){
+    private void init(Bundle savedInstanceState ){
         messagelist_fragment = new Messagelist_Fragment();
 
-        mFragmentList.add(messagelist_fragment);
+        mFragmentList.add(new Profile_Fragment());
         mFragmentList.add(new Character_Fragment());
         mFragmentList.add(new Friendlist_Fragment());
+        mFragmentList.add(messagelist_fragment);
 
         mViewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(),mFragmentList);
         mViewPager.setAdapter(mViewPagerAdapter);
         mViewPager.setCurrentItem(1);
+
     }
 }
