9f8f7c20d7e84d5676516b0162238c8b573f73f3
evility <h35765469@gmail.com>
2017-06-27 15:58:43 +0800

    2017/6/27                    1.建立設定RelativeLayout背景色的array                    2.建立設定各種title Icon的array

--- app/src/main/java/com/cool/user/netty_chatsystem/Chat_Fragment/ProfileFragment/ProfileRecycleViewAdapter.java
+++ app/src/main/java/com/cool/user/netty_chatsystem/Chat_Fragment/ProfileFragment/ProfileRecycleViewAdapter.java
@@ -1,11 +1,13 @@
 package com.cool.user.netty_chatsystem.Chat_Fragment.ProfileFragment;
 
+import android.graphics.Color;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.cool.user.netty_chatsystem.R;
@@ -16,20 +18,23 @@ import com.cool.user.netty_chatsystem.R;
 
 public class ProfileRecycleViewAdapter extends RecyclerView.Adapter<ProfileRecycleViewAdapter.PersonViewHolder> {
     String[] titleNames = {"我的驚喜", "我的收藏", "認證中", "新朋友", "設定"};
-    int[] titleIcons = {R.drawable.action_btn};
+    String[] relativeColors = {"#FF0088", "#FF8800", "#7700FF","#FF3333","#888888" };
+    int[] titleIcons = {R.drawable.profile_surprise, R.drawable.agenda, R.drawable.profile_certification, R.drawable.logo, R.drawable.profile_setting};
     private static ClickListener clickListener;;
 
     public class PersonViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         CardView cv;
-        TextView personName;
-        ImageView personPhoto;
+        TextView profileItemNameTxt;
+        ImageView profileItemIconImg;
+        RelativeLayout profileItemRelativeLayout;
 
         PersonViewHolder(View itemView) {
             super(itemView);
             itemView.setOnClickListener(this);
             cv = (CardView)itemView.findViewById(R.id.cv);
-            personName = (TextView)itemView.findViewById(R.id.person_name);
-            personPhoto = (ImageView)itemView.findViewById(R.id.friendContentImg);
+            profileItemNameTxt = (TextView)itemView.findViewById(R.id.profileItemNameTxt);
+            profileItemIconImg = (ImageView)itemView.findViewById(R.id.profileItemIconImg);
+            profileItemRelativeLayout = (RelativeLayout)itemView.findViewById(R.id.profileItemRelativeLayout);
         }
 
         @Override
@@ -45,15 +50,16 @@ public class ProfileRecycleViewAdapter extends RecyclerView.Adapter<ProfileRecyc
 
     @Override
     public PersonViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
-        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.testcardview, viewGroup, false);
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.resource_profile_recycleview, viewGroup, false);
         PersonViewHolder pvh = new PersonViewHolder(v);
         return pvh;
     }
 
     @Override
     public void onBindViewHolder(PersonViewHolder personViewHolder, int position) {
-        personViewHolder.personName.setText(titleNames[position]);
-        personViewHolder.personPhoto.setImageResource(R.drawable.logo_red);
+        personViewHolder.profileItemNameTxt.setText(titleNames[position]);
+        personViewHolder.profileItemRelativeLayout.setBackgroundColor(Color.parseColor(relativeColors[position]));
+        personViewHolder.profileItemIconImg.setImageResource(titleIcons[position]);
     }
 
     @Override
