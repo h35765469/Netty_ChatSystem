d67163c8f4368998faf779787612582df637c959
evility <h35765469@gmail.com>
2017-06-30 09:33:46 +0800

    2017/6/30                   添加新頁面的UI變數

--- app/src/main/java/com/cool/user/netty_chatsystem/Chat_Listview_Message/ChatAdapter.java
+++ app/src/main/java/com/cool/user/netty_chatsystem/Chat_Listview_Message/ChatAdapter.java
@@ -17,6 +17,8 @@ import com.cool.user.netty_chatsystem.Chat_Fragment.ChatFragment.ChatFragment;
 import com.cool.user.netty_chatsystem.R;
 import com.squareup.picasso.Picasso;
 
+import org.w3c.dom.Text;
+
 import java.io.File;
 import java.util.List;
 
@@ -73,7 +75,7 @@ public class ChatAdapter extends BaseAdapter {
 
         boolean myMsg = chatMessage.getIsme() ;//Just a dummy check
         //to simulate whether it me or other sender
-        setAlignment(holder, myMsg, chatMessage);
+        setAlignment(holder, myMsg, chatMessage, position);
 
         /*if(chatMessage.getMessage() != null) {
             holder.txtMessage.setText(chatMessage.getMessage());
@@ -115,7 +117,7 @@ public class ChatAdapter extends BaseAdapter {
         chatMessages.addAll(messages);
     }
 
-    private void setAlignment(ViewHolder holder, boolean isMe, ChatMessage chatMessage) {
+    private void setAlignment(ViewHolder holder, boolean isMe, ChatMessage chatMessage, int position) {
         if (!isMe) {
             //holder.contentWithBG.setBackgroundResource(R.drawable.in_message_bg);
 
@@ -150,7 +152,20 @@ public class ChatAdapter extends BaseAdapter {
             holder.picMessage.setLayoutParams(layoutParams);*/
             holder.myself.setVisibility(View.GONE);
             holder.other.setVisibility(View.VISIBLE);
-            holder.otherChatTxt.setText(chatMessage.getMessage());
+            holder.friendChatTxt.setVisibility(View.GONE);
+            holder.friendChatImg.setVisibility(View.GONE);
+            if(chatMessage.getFilePath() !=null) {
+                holder.friendChatImg.setVisibility(View.VISIBLE);
+                if(chatMessage.getIsEffect()){
+                    Picasso.with(context).load(R.drawable.gift).into(holder.friendChatImg);
+                }else{
+                    Picasso.with(context).load(new File(chatMessage.getFilePath())).into(holder.friendChatImg);
+                }
+            }else {
+                holder.friendChatTxt.setVisibility(View.VISIBLE);
+                holder.friendChatTxt.setText(chatMessage.getMessage());
+                holder.otherChatTxt.setText(chatMessage.getMessage());
+            }
 
         } else {
             //holder.contentWithBG.setBackgroundResource(R.drawable.out_message_bg);
@@ -185,7 +200,32 @@ public class ChatAdapter extends BaseAdapter {
             holder.picMessage.setLayoutParams(layoutParams);*/
             holder.other.setVisibility(View.GONE);
             holder.myself.setVisibility(View.VISIBLE);
-            holder.myselfChatTxt.setText(chatMessage.getMessage());
+            holder.myChatTxt.setVisibility(View.GONE);
+            holder.myChatImg.setVisibility(View.GONE);
+            if(chatMessage.getFilePath() !=null) {
+                holder.myChatImg.setVisibility(View.VISIBLE);
+                if(chatMessage.getIsEffect()){
+                    holder.myChatImg.setMaxHeight(40);
+                    holder.myChatImg.setMaxWidth(40);
+                    Picasso.with(context).load(R.drawable.gift).into(holder.myChatImg);
+                }else{
+                    Picasso.with(context).load(new File(chatMessage.getFilePath())).into(holder.myChatImg);
+                }
+            }else {
+                holder.myChatTxt.setVisibility(View.VISIBLE);
+                holder.myChatTxt.setText(chatMessage.getMessage());
+            }
+
+            if(position == getCount() - 1) {
+                if (chatMessage.getIsRead() == 1) {
+                    holder.alreadyReadTxt.setVisibility(View.VISIBLE);
+                } else {
+                    holder.alreadyReadTxt.setVisibility(View.GONE);
+                }
+            }else{
+                holder.alreadyReadTxt.setVisibility(View.GONE);
+            }
+
         }
     }
 
@@ -202,6 +242,12 @@ public class ChatAdapter extends BaseAdapter {
         holder.other = (RelativeLayout)v.findViewById(R.id.other);
         holder.myselfChatTxt = (TextView)v.findViewById(R.id.myselfChatTxt);
         holder.otherChatTxt = (TextView)v.findViewById(R.id.otherChatTxt);
+
+        holder.myChatTxt = (TextView)v.findViewById(R.id.myChatTxt);
+        holder.friendChatTxt = (TextView)v.findViewById(R.id.friendChatTxt);
+        holder.alreadyReadTxt = (TextView)v.findViewById(R.id.alreadyReadTxt);
+        holder.myChatImg = (ImageView)v.findViewById(R.id.myChatImg);
+        holder.friendChatImg = (ImageView)v.findViewById(R.id.friendChatImg);
         return holder;
     }
 
@@ -217,6 +263,12 @@ public class ChatAdapter extends BaseAdapter {
         public RelativeLayout other;
         public TextView myselfChatTxt;
         public TextView otherChatTxt;
+
+        public TextView myChatTxt;
+        public TextView friendChatTxt;
+        public TextView alreadyReadTxt;
+        public ImageView myChatImg;
+        public ImageView friendChatImg;
 
     }
 }
